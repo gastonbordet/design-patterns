4b70de6cb72549843354a6953f69d47db9c9c5e7
gbordet <gastonbordet95@gmail.com>
2023-04-16 14:09:32 -0300

    feat: added observer pattern example

--- design-patterns/src/main/java/org/patterns/Main.java
+++ design-patterns/src/main/java/org/patterns/Main.java
@@ -1,5 +1,8 @@
 package org.patterns;
 
+import org.patterns.observer.EmailTopic;
+import org.patterns.observer.EmailTopicSubscriber;
+import org.patterns.observer.Observer;
 import org.patterns.strategy.payment.CreditCard;
 import org.patterns.strategy.payment.Paypal;
 import org.patterns.strategy.payment.Product;
@@ -10,6 +13,8 @@ import org.patterns.strategy.payment.ShoppingCart;
 import org.patterns.strategy.scoreboard.Balloon;
 import org.patterns.strategy.scoreboard.Clown;
 import org.patterns.strategy.scoreboard.Scoreboard;
+
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -24,8 +29,30 @@ public class Main {
 
         shoppingCart.pay(new Paypal());
     }
+
+    public static void observer() {
+        EmailTopic topic = new EmailTopic();
+        List<Observer> observers = List.of(
+                new EmailTopicSubscriber("First observer"),
+                new EmailTopicSubscriber("Second observer"),
+                new EmailTopicSubscriber("Third observer")
+        );
+
+        // Register observer to topic and attach topic to observer
+        observers.stream().forEach(observer -> {
+            topic.register(observer);
+            observer.setSubject(topic);
+        });
+
+        // Check for updates
+        observers.stream().forEach(observer -> observer.update());
+
+        // Update topic
+        topic.postMessage("Hello! This is a new message to all subscribers!");
+    }
+
     public static void main(String[] args) {
         // strategy();
-
+        observer();
     }
 }
---
+++ design-patterns/src/main/java/org/patterns/observer/Subject.java
@@ -0,0 +1,8 @@
+package org.patterns.observer;
+
+public interface Subject {
+    public void register(Observer observer);
+    public void unregister(Observer observer);
+    public void notifyObservers();
+    public Object getUpdate(Observer observer);
+}
