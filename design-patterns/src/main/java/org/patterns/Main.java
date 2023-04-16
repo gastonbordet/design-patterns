a2be1a49df8a1dd88dfde8df33a3a93a03103d2b
gbordet <gastonbordet95@gmail.com>
2023-04-16 13:25:28 -0300

    feat: create pattern methods

--- design-patterns/src/main/java/org/patterns/Main.java
+++ design-patterns/src/main/java/org/patterns/Main.java
@@ -7,10 +7,14 @@ import org.patterns.strategy.scoreboard.Scoreboard;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void strategy() {
         Scoreboard scoreboard = new Scoreboard(new Balloon());
         scoreboard.showScore(10, 5);
         scoreboard.setAlgorithmBase(new Clown());
         scoreboard.showScore(10, 5);
+    }
+    public static void main(String[] args) {
+        // strategy();
+
     }
 }
